f91f669b99a257dd0d290075ca6fcebe25ffb857
Mike Buckley <mikebuckley.canada@gmail.com>
2016-04-17 12:55:47 -0700

    Initial commit of model pattern

---
+++ demo/pdpProductContextMock.js
@@ -0,0 +1,43 @@
+// simple mock for a pre-rendered product context object
+window.context = {
+    "pdpProduct": {
+        "sku": "10415421",
+        "name": "Apple iPad Pro 9.7&#34; 32GB with Wi-Fi - Gold",
+        "regularPrice": 799.9900,
+        "salePrice": 799.9900,
+        "thumbnailImage": "https://multimedia.bbycastatic.ca/multimedia/products/55x55/104/10415/10415421.jpg",
+        "productUrl": "http://www.bestbuy.ca//en-CA/product/apple-apple-ipad-pro-9-7-32gb-with-wi-fi-gold-mlmq2cl-a/10415421.aspx",
+        "ehf": 0.0,
+        "hideSavings": false,
+        "isSpecialDelivery": false,
+        "saleEndDate": null,
+        "additionalMedia": [{
+            "thumbnailUrl": "https://multimedia.bbycastatic.ca/multimedia/products/55x55/104/10415/10415421.jpg",
+            "url": "https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10415/10415421.jpg",
+            "mimeType": "image"
+        }, {
+            "thumbnailUrl": "https://multimedia.bbycastatic.ca/multimedia/products/55x55/104/10415/10415421_1.jpg",
+            "url": "https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10415/10415421_1.jpg",
+            "mimeType": "image"
+        }, {
+            "thumbnailUrl": "https://multimedia.bbycastatic.ca/multimedia/products/55x55/104/10415/10415421_2.jpg",
+            "url": "https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10415/10415421_2.jpg",
+            "mimeType": "image"
+        }, {
+            "thumbnailUrl": "https://multimedia.bbycastatic.ca/multimedia/products/55x55/104/10415/10415421_3.jpg",
+            "url": "https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10415/10415421_3.jpg",
+            "mimeType": "image"
+        }, {
+            "thumbnailUrl": "https://multimedia.bbycastatic.ca/multimedia/products/55x55/104/10415/10415421_4.jpg",
+            "url": "https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10415/10415421_4.jpg",
+            "mimeType": "image"
+        }],
+        "seller": {
+            "name": null,
+            "id": null,
+            "url": ""
+        },
+        "isMarketplace": false,
+        "brandName": "APPLE"
+    }
+};
